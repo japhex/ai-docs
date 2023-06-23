01abcbaf3cc52ffae9c3a16303f9b23fe460ab58
Jamie Lawrence <jamie.lawrence@1e.com>
2023-06-23 15:54:41 +0100

    all sorts of bits

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 // You need to import our styles for the button to look right. Best to import in the root /layout.tsx but this is fine
 import '@uploadthing/react/styles.css'
 
-import { UploadButton } from '../utils/uploadthing'
+import { UploadButton } from '@/utils/uploadthing'
 import { Center } from '@chakra-ui/react'
 
 export default function Home() {
@@ -13,12 +13,10 @@ export default function Home() {
         <UploadButton
           endpoint="pdfUploader"
           onClientUploadComplete={res => {
-            // Do something with the response
             console.log('Files: ', res)
             alert('Upload Completed')
           }}
           onUploadError={(error: Error) => {
-            // Do something with the error.
             alert(`ERROR! ${error.message}`)
           }}
         />
